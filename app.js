d1b049e732e8f2e979c8cf0d1ca3c1ad42f947ae
Shaxadhere <Shaxad.here@gmail.com>
2021-04-08 13:06:55 +0500

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const userRoutes = require('./routes/UserRoutes')
+require('dotenv').config()
+
+//app
+const app = express()
+
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    UseCreateIndex: true
+}).then(() => console.log(`Database connected`))
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+//routes
+app.use('/api', userRoutes)
+
+//listen
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
