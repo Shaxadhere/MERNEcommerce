7b10e49e305be763e8c29f5f1b1a2f3c0f7540b2
Shaxadhere <Shaxad.here@gmail.com>
2021-04-10 11:55:40 +0500

    validate product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -9,16 +9,34 @@ exports.create = (req, res) => {
     form.keepExtensions = true
     form.parse(req, (err, fields, files) => {
         if (err) {
-            res.status(400).json({error: "Image could not be uploaded"})
+            return res.status(400).json({error: "Image could not be uploaded"})
         }
+
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({error: "All fields are required"})
+        }
+
         let product = new Product(fields)
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({error: "Image should be less than 1 mb in size"})
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type;
         }
 
         product.save((err, result) => {
-            if(err){
+            if (err) {
                 res.status(400).json({error: errorHandler(err)})
             }
             res.json({result})
